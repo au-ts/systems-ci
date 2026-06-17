2129f880357950d3f4723bd0af3483cb6bae25e8
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-06-17 16:29:51 +1000

    ts_ci: uboot_image_started => image_started
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- ts_ci/boards.py
+++ ts_ci/boards.py
@@ -26,7 +26,7 @@ MACHINE_QUEUE_BOARDS: dict[str, list[str]] = {
 }
 
 MACHINE_QUEUE_BOARD_OPTIONS: dict[str, dict[str, Any]] = {
-    "serengeti": dict(uboot_image_started=b"Starting kernel ..."),
-    "star64": dict(uboot_image_started=b"Starting kernel ..."),
-    "hifive_p550": dict(uboot_image_started=b"Starting kernel ..."),
+    "serengeti": dict(image_started=b"Starting kernel ..."),
+    "star64": dict(image_started=b"Starting kernel ..."),
+    "hifive_p550": dict(image_started=b"Starting kernel ..."),
 }
